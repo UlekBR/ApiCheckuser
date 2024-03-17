84e8dd5485ce93f8673955956de0d61c53776334
Paulo <127901575+UlekBR@users.noreply.github.com>
2024-03-17 19:19:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ arch=$(uname -m)
 
 if [[ $arch == "x86_64" || $arch == "amd64" || $arch == "x86_64h" ]]; then
     echo "Sistema baseado em x86_64 (64-bit Intel/AMD)"
-    curl -o "/opt/ApiCheckuser/GoCheckuser" ""
+    curl -o "/opt/ApiCheckuser/GoCheckuser" "https://raw.githubusercontent.com/UlekBR/ApiCheckuser/main/GoCheckuser"
 elif [[ $arch == "aarch64" || $arch == "arm64" || $arch == "armv8-a" ]]; then
     echo "Sistema baseado em arm64 (64-bit ARM)"
     curl -o "/opt/ApiCheckuser/GoCheckuser" ""
@@ -19,6 +19,6 @@ else
     echo "Arquitetura não reconhecida: $arch"
 fi
 
-curl -o "/opt/ApiCheckuser/menu.sh" ""
+curl -o "/opt/ApiCheckuser/menu.sh" "https://raw.githubusercontent.com/UlekBR/ApiCheckuser/main/menu.sh"
 
 
